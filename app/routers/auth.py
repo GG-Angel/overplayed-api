8de4556bcaa7dbca5162b762c50f324501f46c26
Gael <angel.ga@northeastern.edu>
2025-11-30 22:31:31 -0500

    spotify envs

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,15 @@
+import os
 from fastapi import APIRouter
 
 router = APIRouter(
   prefix='/auth',
   tags=['auth']
 )
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+SPOTIFY_SCOPES = "playlist-read-private playlist-modify-private playlist-modify-public"
 
 @router.get("/login")
 def login():
