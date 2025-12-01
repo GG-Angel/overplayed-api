81b0acf8790a586aed8b20fccd0074ea391fd69b
Gael <angel.ga@northeastern.edu>
2025-11-30 23:33:44 -0500

    oauth basics

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter, FastAPI
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from .routers import auth
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+
+secret_key = os.getenv("SESSION_SECRET_KEY")
+if not secret_key:
+    raise RuntimeError("Environment variable 'SESSION_SECRET_KEY' is not set")
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=secret_key,
+)
 
 app.include_router(auth.router)
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def index():
+  return {"message": "Hello, World!"}
+
+@app.get("/test")
+def test(request: Request):
+    try:
+        token_info = auth.get_token_info(request)
+        return {"logged_in": True, "token_info": token_info}
+    except Exception as e:
+        print(e)
+        return RedirectResponse(url="/auth/login")
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,45 @@
 import os
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.datastructures import URL
+from fastapi.responses import RedirectResponse
+from spotipy.oauth2 import SpotifyOAuth
 
 router = APIRouter(
-  prefix='/auth',
-  tags=['auth']
+    prefix='/auth',
+    tags=['auth']
 )
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 SPOTIFY_SCOPES = "playlist-read-private playlist-modify-private playlist-modify-public"
+SESSION_TOKEN_INFO = "token_info"
 
 @router.get("/login")
-def login():
-    return {"message": "Authentication endpoint"}
+def login(request: Request):
+    sp_oauth = create_spotify_oauth(request.url_for("callback"))
+    auth_url = sp_oauth.get_authorize_url()
+    return RedirectResponse(auth_url)
+
+@router.get("/callback")
+def callback(request: Request):
+    sp_oauth = create_spotify_oauth(request.url_for("callback"))
+    request.session.clear()
+    code = request.query_params.get("code")
+    token_info = sp_oauth.get_access_token(code)
+    request.session[SESSION_TOKEN_INFO] = token_info
+    return 'redirect'
+
+
+def create_spotify_oauth(redirect_uri: URL) -> SpotifyOAuth:
+    return SpotifyOAuth(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        redirect_uri=redirect_uri,
+        scope=SPOTIFY_SCOPES
+    )
+
+def get_token_info(request: Request):
+    token_info = request.session.get(SESSION_TOKEN_INFO, None)
+    if not token_info:
+        raise Exception("User not logged in")
