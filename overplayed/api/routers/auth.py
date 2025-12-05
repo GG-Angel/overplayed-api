2833b18b9ebab8c32abf72440d01cee56d629199
Gael Angel <angel.ga@northeastern.edu>
2025-12-05 02:09:02 -0500

    feat: oauth, get playlist and tracks (#1)
    * setup uv
    
    * setup auth endpoint
    
    * spotify envs
    
    * add spotipy
    
    * oauth basics
    
    * working response
    
    * create config
    
    * move settings
    
    * move funcs
    
    * move to api folder
    
    * init
    
    * rename folder
    
    * get owned playlists
    
    * lint
    
    * get user id
    
    * get playlist songs
    
    * fetch playlist tracks

---
+++ overplayed/api/routers/auth.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+from overplayed.config import settings
+from overplayed.api.dependencies import create_spotify_oauth
+
+router = APIRouter()
+
+
+@router.get("/login")
+def login(request: Request):
+    sp_oauth = create_spotify_oauth(request)
+    auth_url = sp_oauth.get_authorize_url()
+    return RedirectResponse(auth_url)
+
+
+@router.get("/callback")
+def callback(request: Request):
+    sp_oauth = create_spotify_oauth(request)
+    request.session.clear()
+    code = request.query_params.get("code")
+    token_info = sp_oauth.get_access_token(code)
+    request.session[settings.session_token_info] = token_info
+    return RedirectResponse(url="/")
---
+++ overplayed/api/routers/playlist.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+import spotipy
+from overplayed.api.dependencies import (
+    fetch_user_playlists,
+    fetch_playlist_tracks,
+    get_token_info,
+    get_user_id,
+)
+
+router = APIRouter()
+
+
+@router.get("/")
+def get_playlists(request: Request):
+    try:
+        token_info = get_token_info(request)
+    except Exception:
+        return RedirectResponse(url="/auth/login")
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    return fetch_user_playlists(sp, get_user_id(sp))
+
+
+@router.get("/{playlist_id}")
+def get_tracks(playlist_id: str, request: Request):
+    try:
+        token_info = get_token_info(request)
+    except Exception:
+        return RedirectResponse(url="/auth/login")
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    return fetch_playlist_tracks(sp, playlist_id)
